1525980e6ef704385d3164fda1eafc5d93468c9d
Piotr Stawarski <piotr.stawarski@zerodowntime.pl>
2019-03-26 16:45:26 +0100

    smart pre-stop

--- pre-stop.sh
+++ pre-stop.sh
@@ -1,4 +1,30 @@
 #!/bin/sh
 
-nodetool drain
-nodetool stopdaemon
+if [ -z ${HOSTNAME+x} ]; then
+  HOSTNAME=$(hostname -s)
+fi
+
+if [ -d /run/secrets/kubernetes.io/serviceaccount ]; then
+  METADATA_NAMESPACE=$(cat /run/secrets/kubernetes.io/serviceaccount/namespace)
+  BEARER_TOKEN=$(cat /run/secrets/kubernetes.io/serviceaccount/token)
+  STATEFULSET_NAME="${HOSTNAME%-*}"
+  POD_ORDINAL="${HOSTNAME##*-}"
+  JSONFILE="$(mktemp)"
+  curl -s -o "$JSONFILE" \
+    --cacert "/run/secrets/kubernetes.io/serviceaccount/ca.crt" \
+    --header "Authorization: Bearer $BEARER_TOKEN" \
+    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_SERVICE_PORT/apis/apps/v1/namespaces/$METADATA_NAMESPACE/statefulsets/$STATEFULSET_NAME"
+  if [ "$(jq -r .kind $JSONFILE)" = "StatefulSet" ]; then
+    SPEC_REPLICAS="$(jq .spec.replicas $JSONFILE)"
+    if [ "$POD_ORDINAL" -ge "$SPEC_REPLICAS" ]; then
+      while ! nodetool decommission; do
+        if nodetool netstats | grep -i Mode | grep -i DRAINED; then
+          exec nodetool stopdaemon
+        fi
+        sleep 10
+      done
+    fi
+  fi
+fi
+
+exec nodetool drain
